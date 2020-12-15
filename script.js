28c05fb6866b57d28298a93752efa1e68787c32c
farleysm8909 <farleysm8909@uwec.edu>
2020-12-15 10:58:52 -0600

    finished assignment

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
+   let missionTarget = document.getElementById("missionTarget");
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         let index = 0;
+         let data = json[index];
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${data.name}</li>
+            <li>Diameter: ${data.diameter}</li>
+            <li>Star: ${data.star}</li>
+            <li>Distance from Earth: ${data.distance}</li>
+            <li>Number of Moons: ${data.moons}</li>
+         </ol>
+         <img src="${data.image}">
+         `;
+      });
+   });
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event){
       event.preventDefault();
@@ -8,6 +26,14 @@ window.addEventListener("load", function() {
       let fuelLevel = document.getElementById("fuelLevel");
       let cargoMass = document.getElementById("cargoMass");
       let stringRegex = /^[a-zA-Z]+$/;
+
+      let faultyItems = document.getElementById("faultyItems");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let launchStatus = document.getElementById("launchStatus");
+
       if (pilotName.value.length < 1 || copilotName.value.length < 1 || fuelLevel.value === "" || cargoMass.value === "") {
          alert("All fields are required!");
          // event.preventDefault();
@@ -15,11 +41,26 @@ window.addEventListener("load", function() {
          alert("Please enter valid input");
          // event.preventDefault();
       } else {
-         alert("else");
+         faultyItems.style.visibility = "visible";
+         pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
+         copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready for launch.`;
+         if (fuelLevel.value < 10000) {
+            fuelStatus.innerHTML = `Fuel level too low for journey.`;
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+         }
+         if (cargoMass.value > 10000) {
+            cargoStatus.innerHTML = `Too much mass for launch.`;
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+         }
+         if (cargoMass.value < 10000 && fuelLevel.value > 10000) {
+            launchStatus.innerHTML = `Shuttle is ready for launch`;
+            launchStatus.style.color = "green";
+         }
       }
    });
-});
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+   /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${}</li>
@@ -30,3 +71,5 @@ window.addEventListener("load", function() {
 </ol>
 <img src="${}">
 */
+});
+
